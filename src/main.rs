857a4d6c1d79a4746c2e38f8ed5cdf31fa410e7e
erikwilbert <erik.wilbert@ui.ac.id>
2026-05-11 19:36:04 +0700

    Change identifier

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
     
         thread::sleep(ten_millis);
 
-        println!("In Ade’s Computer [129500004y]. Message received: {:?}", message);
+        println!("In Erik's Computer [2406495376]. Message received: {:?}", message);
         Ok(())
     }
 
